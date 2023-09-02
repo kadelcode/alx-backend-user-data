cb18ec28ff8f641b6c5bc413d5f834548481737e
Ayodele Kadiri <kadelcode@gmail.com>
2023-09-02 19:36:20 +0000

    filtered logger

--- filtered_logger.py
+++ filtered_logger.py
@@ -1,116 +1,87 @@
 #!/usr/bin/env python3
-"""Handling personal data with the logging module"""
+"""
+Personal data
+"""
 
-from typing import List
-import csv
+
 import logging
-import mysql.connector
-import re
 import os
+import re
+from typing import List
+import mysql.connector
 
-# PII_FIELDS = ('name', 'email', 'phone', 'ssn', 'password')
-
-
-def get_db() -> mysql.connector.connection.MySQLConnection:
-    """Returns a connector to the database"""
-    db_name = os.getenv("PERSONAL_DATA_DB_NAME")
-    db_username = os.getenv("PERSONAL_DATA_DB_USERNAME")
-    db_password = os.getenv("PERSONAL_DATA_DB_PASSWORD")
-    db_host = os.getenv("PERSONAL_DATA_DB_HOST")
-    db = mysql.connector.connect(
-        database=db_name if db_name else 'my_db',
-        host=db_host if db_host else 'localhost',
-        user=db_username if db_username else 'root',
-        password=db_password if db_password else 'root'
-    )
-
-    return db
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
-with open('user_data.csv', 'r') as f:
-    PII_FIELDS: tuple = ()
-    reader = csv.reader(f)
-    fields = next(reader)
-    PII = ['name', 'address', 'email', 'ssn', 'passport', 'dl',
-           'cc', 'birth', 'dob', 'born', 'phone', 'telephone',
-           'vin', 'username', 'password', 'mac', 'ip']
-    for field in fields:
-        if field in PII:
-            if len(PII_FIELDS) < 5:
-                PII_FIELDS += (field,)
-            else:
-                break
-
-
-def get_logger() -> logging.Logger:
-    """returns a logger for logging user data"""
-    user_logger = logging.Logger("user_data")
-    user_logger.setLevel(logging.INFO)
-    user_logger.propagate = False
-
-    handler = logging.StreamHandler()
-    handler.setFormatter(RedactingFormatter(PII_FIELDS))
-    user_logger.addHandler(handler)
-    return user_logger
-
-
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, separator: str) -> str:
-    """ returns the log message obfuscated """
-    temp = message
-    for field in fields:
-        temp = re.sub(field + "=.*?" + separator,
-                      field + "=" + redaction + separator, temp)
-    return temp
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
+    """ Replacing """
+    for f in fields:
+        message = re.sub(rf"{f}=(.*?)\{separator}",
+                         f'{f}={redaction}{separator}', message)
+    return message
 
 
 class RedactingFormatter(logging.Formatter):
-    """ Redacting Formatter class
-    """
+    """ RedactingFormatter class. """
 
     REDACTION = "***"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
     def __init__(self, fields: List[str]):
-        """inherits init from Formatter"""
-        super(RedactingFormatter, self).__init__(self.FORMAT)
+        """ Init """
         self.fields = fields
+        super(RedactingFormatter, self).__init__(self.FORMAT)
 
     def format(self, record: logging.LogRecord) -> str:
-        """
-        uses the format() method from parent and
-        applies and extra filter with filter_datum()
-        """
-        log: logging.Formatter = super().format(record)
-        return filter_datum(self.fields, self.REDACTION, log, self.SEPARATOR)
+        """ Format """
+        return filter_datum(self.fields, self.REDACTION,
+                            super().format(record), self.SEPARATOR)
 
 
-def main():
+def get_logger() -> logging.Logger:
+    """ Implementing a logger.
     """
-    Use all the above functions
-    to read from a database and log the
-    rows from the 'users' table, filtering
-    personal data.
+
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(PII_FIELDS))
+    logger.addHandler(handler)
+    return logger
+
+
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """ Implement db conectivity
+    """
+    psw = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    username = os.environ.get('PERSONAL_DATA_DB_USERNAME', "root")
+    host = os.environ.get('PERSONAL_DATA_DB_HOST', 'localhost')
+    db_name = os.environ.get('PERSONAL_DATA_DB_NAME')
+    conn = mysql.connector.connect(
+        host=host,
+        database=db_name,
+        user=username,
+        password=psw)
+    return conn
+
+
+def main() -> None:
+    """ Implement a main function
     """
     db = get_db()
     cursor = db.cursor()
-
-    cursor.execute('SELECT * FROM users;')
-    rows = cursor.fetchall()
-
-    logger = get_logger()
-    field_names = [i[0] for i in cursor.description]
-
-    for row in rows:
-        message = ''
-        for field in range(len(row)):
-            message += f'{field_names[field]}={row[field]};'
-        logger.info(message)
-
+    cursor.execute("SELECT * FROM users;")
+    for row in cursor:
+        message = f"name={row[0]}; email={row[1]}; phone={row[2]}; " +\
+            f"ssn={row[3]}; password={row[4]};ip={row[5]}; " +\
+            f"last_login={row[6]}; user_agent={row[7]};"
+        print(message)
     cursor.close()
     db.close()
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
