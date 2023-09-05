3a4120c5f720cc904c56af383f85e143eb072f64
Ayodele Kadiri <kadelcode@gmail.com>
2023-09-05 16:23:46 +0000

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -13,6 +13,10 @@ app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
+@app.errorhandler(401)
+def resource_not_found(e):
+    """ Unauthorized handler"""
+    return jsonify(error="Unauthorized"), 401
 
 @app.errorhandler(404)
 def not_found(error) -> str:
--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -4,6 +4,13 @@
 from flask import jsonify, abort
 from api.v1.views import app_views
 
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized():
+    """ GET /api/v1/unauthorized
+    Return:
+      - the error of the API
+    """
+    abort(401)
 
 @app_views.route('/status', methods=['GET'], strict_slashes=False)
 def status() -> str:
