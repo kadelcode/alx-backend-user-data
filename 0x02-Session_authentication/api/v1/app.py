8aa7fb12e4431f911e1fc5b1b14da0199ae5685f
Ayodele Kadiri <kadelcode@gmail.com>
2023-09-08 17:02:38 +0000

    update file

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -1,76 +1,62 @@
 #!/usr/bin/env python3
 """
-Route module for the API
+    Module Author implemenation
 """
+from flask import request
+from typing import List, TypeVar
 from os import getenv
-from api.v1.views import app_views
-from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
-import os
 
 
-app = Flask(__name__)
-app.register_blueprint(app_views)
-CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
-
-
-auth = getenv("AUTH_TYPE", None)
-
-if auth:
-    if auth == 'basic_auth':
-        from api.v1.auth.basic_auth import BasicAuth
-        auth = BasicAuth()
-    elif auth == 'session_auth':
-        from api.v1.auth.session_auth import SessionAuth
-        auth = SessionAuth()
-    elif auth == 'session_exp_auth':
-        from api.v1.auth.session_exp_auth import SessionExpAuth
-        auth = SessionExpAuth()
-    else:
-        from api.v1.auth.auth import Auth
-        auth = Auth()
-
-
-@app.errorhandler(401)
-def resource_not_found(e):
-    """ Unauthorized handler"""
-    return jsonify(error="Unauthorized"), 401
-
-
-@app.errorhandler(403)
-def f_Forbidden(e):
-    """ Unauthorized handler"""
-    return jsonify(error="Forbidden"), 403
-
-
-@app.errorhandler(404)
-def not_found(error) -> str:
-    """ Not found handler
+class Auth:
     """
-    return jsonify({"error": "Not found"}), 404
-
-
-@app.before_request
-def validate_auth() -> None:
-    """ Validates authentication if needed
+        Author class implementation
     """
-    if auth:
-        request.current_user = auth.current_user(request)
-        excluded_paths = [
-            '/api/v1/status/',
-            '/api/v1/unauthorized/',
-            '/api/v1/forbidden/',
-            '/api/v1/auth_session/login/'
-            ]
-        if auth.require_auth(request.path, excluded_paths):
-            if auth.authorization_header(request) is None and\
-                    auth.session_cookie(request) is None:
-                abort(401)
-            if auth.current_user(request) is None:
-                abort(403)
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """_summary_
 
+        Args:
+            path (str): _description_
+            excluded_paths (List[str]): _description_
 
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
+        Returns:
+            bool: _description_
+        """
+        if path is None or not excluded_paths:
+            return True
+        for excl_path in excluded_paths:
+            if excl_path.endswith('*') and path.startswith(excl_path[:-1]):
+                return False
+            elif excl_path in {path, path + '/'}:
+                return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """that returns None - request will be the Flask request object
+
+        Args:
+            request (_type_, optional): _description_. Defaults to None.
+
+        Returns:
+            str: _description_
+        """
+        if request is None:
+            return None
+        if request.headers.get('Authorization'):
+            return request.headers.get('Authorization')
+        return None
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """_summary_
+
+        Returns:
+            _type_: _description_
+        """
+        return None
+
+    def session_cookie(self, request=None):
+        """ Returns a cookie value from a request
+        """
+        if request is None:
+            return None
+        cookie = getenv("SESSION_NAME", None)
+        return request.cookies.get(cookie)
